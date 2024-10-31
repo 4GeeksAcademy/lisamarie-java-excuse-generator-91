3bc172259703d8b004373c5a4b59bd497ce9d9c5
lisamarie245 <108356315+lisamarie245@users.noreply.github.com>
2024-10-31 10:38:25 +0000

    Java Excuse Generator - Lisa Moreno

--- src/app.js
+++ src/app.js
@@ -2,10 +2,22 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let name = ["the dog", "my granma", "his turtle", "my bird"];
+  let action = ["eat", "pissed", "crushed", "broked"];
+  let place = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let indicename = Math.floor(Math.random() * name.length);
+  let indiceaction = Math.floor(Math.random() * action.length);
+  let indiceplace = Math.floor(Math.random() * place.length);
+
+  document.querySelector("#excuse").innerHTML =
+    name[indicename] + " " + action[indiceaction] + " " + place[indiceplace];
+  console.log("Hello rigo from the console!");
 };
